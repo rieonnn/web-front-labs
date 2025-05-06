a82fabac3153275271a7a066322e40d4e7e9e765
Ирина Андреева <rbbzzns@gmail.com>
2025-05-06 12:27:15 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,85 @@
+const books = [
+    {
+        author: 'Стивен Кинг',
+        name:'Оно',
+        publicationYear: 1986,
+        price: 899,
+        bestseller: true,
+        inStock: true
+    },
+
+    {
+        author: 'Стивен Кинг',
+        name:'Обезьяна',
+        publicationYear: 1980,
+        price: 749,
+        bestseller: false,
+        inStock: true
+    },
+
+    {
+        author: 'Стивен Кинг',
+        name:'Тёмная башня',
+        publicationYear: 1970,
+        price: 1299,
+        bestseller: false,
+        inStock: true
+    },
+
+    {
+        author: 'Джоан Роулинг',
+        name: 'Гарри Поттер и философский камень',
+        publicationYear: 1997,
+        price: 999,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: 599,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джером Д. Сэлинджер',
+        name: 'Над пропастью во ржи',
+        publicationYear: 1951,
+        price: 699,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Фрэнсис Скотт Фицджеральд',
+        name: 'Великий Гэтсби',
+        publicationYear: 1925,
+        price: 549,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 799,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Анна Каренина',
+        publicationYear: 1877,
+        price: 649,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 599,
+        bestseller: true,
+        inStock: true
+    }
+];
